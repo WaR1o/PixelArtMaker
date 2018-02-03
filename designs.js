3de841608364d1efc8843f4d45b2cda46d15608a
WaR1o <shok163@gmail.com>
2018-02-03 18:28:36 +0400

    Function for constructing paintable grid created.

--- designs.js
+++ designs.js
@@ -1,10 +1,31 @@
 // Select color input
+const colorPicker = $('#colorPicker');
 // Select size input
+const sizePicker = $('#sizePicker');
+const pixelCanvas = $('#pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
+$('#submitGrid').on('click', (e) => {
+    e.preventDefault();
+    pixelCanvas.empty();
+    const gridHeight = sizePicker[0][0].value;
+    const gridWidth = sizePicker[0][1].value;
+    makeGrid(gridHeight, gridWidth);
+});
 
-function makeGrid() {
+function makeGrid(height, width) {
+    for (let row = 1; row <= height; row++) {
+        let tableRow = `<tr class="row" id="row${row}"></tr>`;
+        $('#pixelCanvas').append(tableRow);
+        for (let col = 1; col <= width; col++) {
+            let tableCol = `<td class="cell" id="cell${row}-${col}"></td>`;
+            $(`#row${row}`).append(tableCol);
+        }
+    }
 
-// Your code goes here!
-
+    $('.cell').on('click', (e) => {
+        let id = e.target.id;
+        $(`#${id}`).css('background', colorPicker.val());     
+        setTimeout(waitForGrid, 1000);
+    });
 }
